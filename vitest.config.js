3559c94748f209ce6403d75fbb6381e71beb87de
王昱 <wangyu2021@foxmail.com>
2025-07-09 09:38:56 +0300

    feat: configure Vitest for unit testing

---
+++ vitest.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    coverage: {
+      provider: 'c8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src'],
+      all: true,
+    },
+  },
+});
