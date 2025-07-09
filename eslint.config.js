355c3df8e38a0ac1c5267c750fc8bd45a7191d50
王昱 <wangyu2021@foxmail.com>
2025-07-09 11:14:32 +0300

    feat: Add eslint-plugin-import and update ESLint configuration rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,15 @@
 import globals from 'globals';
 import js from '@eslint/js';
 import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import importPlugin from 'eslint-plugin-import';
 
 export default [
   js.configs.recommended,
 
   {
+    plugins: {
+      import: importPlugin,
+    },
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
@@ -15,7 +19,29 @@ export default [
         ...globals.node,
       },
     },
-    rules: {},
+    rules: {
+      'import/no-duplicates': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
   },
 
   // This must be the last item.
