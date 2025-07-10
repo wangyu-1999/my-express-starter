4706d6c4b240de9dfb6acc27d5e163e1692f915f
王昱 <wangyu2021@foxmail.com>
2025-07-10 12:27:26 +0300

    feat: add notes routes to API

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import greetingRoutes from './greeting.routes.js';
+import notesRoutes from './notes.routes.js';
 
 const router = Router();
 
 router.use('/', greetingRoutes);
+router.use('/', notesRoutes);
 
 export default router;
