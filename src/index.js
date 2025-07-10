65625f6ea5df40fb62b55863723c3eaafe649271
王昱 <wangyu2021@foxmail.com>
2025-07-10 12:01:13 +0300

    feat: Refactor PostgreSQL connection handling and server startup logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import app from './app.js';
+import { postgresConnection } from './db/postgres.js';
 import config from './utils/config.js';
 import logger from './utils/logger.js';
 
-app.listen(config.PORT, config.HOST, () => {
-  logger.info(`Server is running on http://${config.HOST}:${config.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await postgresConnection();
+    app.listen(config.PORT, config.HOST, () => {
+      logger.info(`Server is running on http://${config.HOST}:${config.PORT}`);
+    });
+  } catch {
+    process.exit(1);
+  }
+};
+
+startServer();
