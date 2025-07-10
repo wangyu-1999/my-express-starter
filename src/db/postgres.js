efd6eb96e3b102a51c0595e8d0d2b2e00d425277
王昱 <wangyu2021@foxmail.com>
2025-07-10 12:01:13 +0300

    feat: Refactor PostgreSQL connection handling and server startup logic

--- src/db/postgres.js
+++ src/db/postgres.js
@@ -5,15 +5,12 @@ import logger from '../utils/logger.js';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
-const postgresConnection = async () => {
+export const sequelize = new Sequelize(process.env.DATABASE_URL);
+export const postgresConnection = async () => {
   try {
     await sequelize.authenticate();
     logger.info('Connection has been established successfully.');
-    sequelize.close();
   } catch (error) {
     logger.error('Unable to connect to the database:', error);
   }
 };
-
-postgresConnection();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import app from './app.js';
+import { postgresConnection } from './db/postgres.js';
 import config from './utils/config.js';
 import logger from './utils/logger.js';
 
-app.listen(config.PORT, () => {
-  logger.info(`Server is running on http://localhost:${config.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await postgresConnection();
+    app.listen(config.PORT, () => {
+      logger.info(`Server is running on http://localhost:${config.PORT}`);
+    });
+  } catch {
+    process.exit(1);
+  }
+};
+
+startServer();
